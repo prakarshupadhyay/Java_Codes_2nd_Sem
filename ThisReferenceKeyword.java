6faeaf47c9664088fa064407c7f67cd633353f6c
prakarshupadhyay <upadhyayprakarsh@gmail.com>
2026-09-08 08:50:56 +0530

    Java Codes written in 2nd Sem

---
+++ ThisReferenceKeyword.java
@@ -0,0 +1,33 @@
+public class ThisReferenceKeyword {
+    static class Box{
+        int length;
+        int breadth;
+        int height;
+        Box(int dimension){
+            this.length = dimension;
+            this.breadth = dimension;
+            this.height = dimension;
+        }
+        Box(int length, int breadth, int height){
+            this.length = length;
+            this.breadth = breadth;
+            this.height = height;
+        }
+        void volume(){
+            int volume = length * breadth * height;
+            System.out.println("Volume of Box is: " + volume);
+        }
+        
+    }
+    public static void main(String[] args) {
+        Box b1 = new Box(10,30,50);
+        Box b2 = new Box(10, 20, 30);
+        Box b3 = new Box(10, 20, 30);
+        Box b4 = new Box(10);
+        b1.volume();
+        b2.volume();
+        b3.volume();
+        b4.volume();
+    }
+    
+}
